89a856fbfcfae6dff71e49a9dfef7973eef7e0e6
khskhs6991 <khskhs6991@naver.com>
2023-01-02 19:31:46 +0900

    index html change

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "https://khskhs6991.github.io/yolo_react/src/App.jsx";
-import "https://khskhs6991.github.io/yolo_react/src/style/index.css";
+import App from "./App.jsx";
+import "./style/index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
